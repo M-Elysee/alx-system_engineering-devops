d6e40d9e3689ed6c6327e4c411c4adf41ad1d338
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-09-26 16:30:18 +0000

    0x0C. Web server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,10 @@
+# a puppet that installs a Nginx server in a web server
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update -qq; sudo apt-get -y install nginx -qq;' \
+  'sudo bash -c "echo -e 'Hello World!' > /var/www/html/index.nginx-debian.html;"'\
+  'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;'\
+  '/" /etc/nginx/sites-available/default;'\
+  'sudo service nginx start'
+}
