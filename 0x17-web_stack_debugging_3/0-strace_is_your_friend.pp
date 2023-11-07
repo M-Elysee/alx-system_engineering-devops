5201a2130e96c7e611e1ef6efec5db1700e7a763
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-11-07 17:15:47 +0000

    0x17. Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# a puppet code that help to solve our problem of apache that returns 
+# errot code 500
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
