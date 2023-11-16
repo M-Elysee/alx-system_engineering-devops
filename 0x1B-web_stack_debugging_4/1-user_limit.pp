f5a4a3fc871ee44df4994f5ca8ea3d4cae887d72
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-11-16 11:24:06 +0000

    0x1B. Web stack debugging #4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+# a puppet that enables a user holberton to login and pen files
+
+# increase the limit for hard files
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase limit for soft files.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
